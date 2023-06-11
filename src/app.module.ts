890eae7cd9f8558373a02ded768b7b84ba7e3f2d
João Pegoraro <hahmraro@tuta.io>
2023-06-11 15:13:28 -0300

    Adicionei middleware de autenticação do firebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthMiddleware } from './auth/auth.middleware';
 import { CampeonatosModule } from './campeonatos/campeonatos.module';
 import { ClassificacaoModule } from './classificacao/classificacao.module';
+import { NoticiasModule } from './noticias/noticias.module';
+import { PartidaModule } from './partida/partida.module';
 import { PosicaoModule } from './posicao/posicao.module';
 import { RodadaModule } from './rodada/rodada.module';
-import { PartidaModule } from './partida/partida.module';
-import { NoticiasModule } from './noticias/noticias.module';
 
 @Module({
   imports: [
@@ -29,4 +30,10 @@ import { NoticiasModule } from './noticias/noticias.module';
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes({
+      path: '*', method: RequestMethod.ALL
+    });
+  }
+}
